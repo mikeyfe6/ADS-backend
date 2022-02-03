6afdb6c17aa90c654e19ef7410a34f1080b4001b
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2022-02-03 05:15:55 +0100

    updated db settings

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,20 +1,19 @@
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST"),
-        port: env("DATABASE_PORT"),
-        database: env("DATABASE_NAME"),
-        username: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
-        ssl: {
-          rejectUnauthorized: false,
-        },
+  connection: {
+    client: "postgres",
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false,
       },
-      options: {},
     },
+    debug: false,
   },
 });
