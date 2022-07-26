fc20442398671b2cef914ea1b3514f58f34360ad
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2022-07-26 13:08:03 +0200

    fixed apiTokenSalt

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '0c36eaf625d67b23090746fb5f049250'),
+    secret: env("ADMIN_JWT_SECRET", "0c36eaf625d67b23090746fb5f049250"),
   },
+  apiToken: { salt: env("API_TOKEN_SALT") },
 });
