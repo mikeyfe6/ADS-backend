8951fc402400024e6b8636f718894d71d98ae8d4
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2024-01-06 19:18:03 +0100

    testing w/ other changes

--- config/admin.js
+++ config/admin.js
@@ -3,4 +3,9 @@ module.exports = ({ env }) => ({
     secret: env("ADMIN_JWT_SECRET", "0c36eaf625d67b23090746fb5f049250"),
   },
   apiToken: { salt: env("API_TOKEN_SALT") },
+  transfer: {
+    token: {
+      salt: env("TRANSFER_TOKEN_SALT"),
+    },
+  },
 });
